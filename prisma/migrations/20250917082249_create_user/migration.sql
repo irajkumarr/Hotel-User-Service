e5ebf3c198e425511379d4d21d6cf9b929a04a29
Raj Kumar Timalsina <rajkumar.pc009@gmail.com>
2025-09-17 14:08:35 +0545

    changes in user schema to have token verification

--- prisma/migrations/20250917082249_create_user/migration.sql
+++ prisma/migrations/20250917082249_create_user/migration.sql
@@ -11,7 +11,11 @@ CREATE TABLE "public"."users" (
     "phone_number" TEXT,
     "role" "public"."Role" NOT NULL DEFAULT 'USER',
     "profile_image" TEXT,
-    "isVerified" BOOLEAN NOT NULL DEFAULT true,
+    "is_verified" BOOLEAN NOT NULL DEFAULT false,
+    "verification_token" TEXT,
+    "verification_token_expiry" TIMESTAMP(3),
+    "reset_password_token" TEXT,
+    "reset_password_expiry" TIMESTAMP(3),
     "last_login" TIMESTAMP(3),
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL,
@@ -24,3 +28,6 @@ CREATE UNIQUE INDEX "users_email_key" ON "public"."users"("email");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "users_phone_number_key" ON "public"."users"("phone_number");
+
+-- CreateIndex
+CREATE INDEX "users_email_id_idx" ON "public"."users"("email", "id");
